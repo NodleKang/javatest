1f235cc6485294e48ca885daaaf1651e24701f48
Nodle Kang <nodlekang@gmail.com>
2023-07-12 12:26:45 +0900

    add convert bufferedReader

--- util/MyJson.java
+++ util/MyJson.java
@@ -2,9 +2,40 @@ package util;
 
 import com.google.gson.*;
 
+import java.io.BufferedReader;
 import java.util.LinkedList;
 
 public class MyJson {
+
+    /**
+     * BufferedReader를 JsonObject로 변환
+     * @param br
+     * @return
+     */
+    public static JsonObject convertBodyToJsonObject(BufferedReader br) {
+        Gson gson = new GsonBuilder()
+                .serializeNulls()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .setPrettyPrinting()
+                .create();
+        JsonObject jsonObj = gson.fromJson(br, JsonObject.class);
+        return jsonObj;
+    }
+
+    /**
+     * BufferedReader를 JsonArray로 변환
+     * @param br
+     * @return
+     */
+    public static JsonArray convertBodyToJsonArray(BufferedReader br) {
+        Gson gson = new GsonBuilder()
+                .serializeNulls()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .setPrettyPrinting()
+                .create();
+        JsonArray jsonArray = gson.fromJson(br, JsonArray.class);
+        return jsonArray;
+    }
 
     /**
      * String을 JsonObject로 변환
@@ -12,6 +43,7 @@ public class MyJson {
     public static JsonObject convertStringToJsonObject(String jsonStr) {
         Gson gson = new GsonBuilder()
                 //.excludeFieldsWithoutExposeAnnotation() // @Expose 어노테이션이 붙어있지 않은 필드는 JSON을 변환 안 함
+                .serializeNulls() // null 값도 JSON 변환
                 .setDateFormat("yyyy-MM-dd HH:mm:ss") // 날짜 타입을 json으로 담을 때의 형식
                 .setPrettyPrinting() // gson 데이터를 보기 좋게 출력
                 .create();
@@ -25,6 +57,7 @@ public class MyJson {
     public static String convertJsonObjectToString(JsonObject jsonObj) {
         Gson gson = new GsonBuilder()
                 //.excludeFieldsWithoutExposeAnnotation() // @Expose 어노테이션이 붙어있지 않은 필드는 JSON을 변환 안 함
+                .serializeNulls() // null 값도 JSON 변환
                 .setDateFormat("yyyy-MM-dd HH:mm:ss") // 날짜 타입을 json으로 담을 때의 형식
                 .setPrettyPrinting() // gson 데이터를 보기 좋게 출력
                 .create();
@@ -38,6 +71,7 @@ public class MyJson {
     public static JsonArray convertStringToJsonArray(String jsonStr) {
         Gson gson = new GsonBuilder()
                 //.excludeFieldsWithoutExposeAnnotation() // @Expose 어노테이션이 붙어있지 않은 필드는 JSON을 변환 안 함
+                .serializeNulls() // null 값도 JSON 변환
                 .setDateFormat("yyyy-MM-dd HH:mm:ss") // 날짜 타입을 json으로 담을 때의 형식
                 .setPrettyPrinting() // gson 데이터를 보기 좋게 출력
                 .create();
@@ -51,6 +85,7 @@ public class MyJson {
     public static String convertJsonArrayToString(JsonArray jsonArray) {
         Gson gson = new GsonBuilder()
                 //.excludeFieldsWithoutExposeAnnotation() // @Expose 어노테이션이 붙어있지 않은 필드는 JSON을 변환 안 함
+                .serializeNulls()
                 .setDateFormat("yyyy-MM-dd HH:mm:ss") // 날짜 타입을 json으로 담을 때의 형식
                 .setPrettyPrinting() // gson 데이터를 보기 좋게 출력
                 .create();
